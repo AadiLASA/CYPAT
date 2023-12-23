ceac4cffb9fb44c0a99702648d44d95c920077bd
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2023-12-23 12:28:47 -0500

    Update and rename POLICYWIPE.bat to POLICYWIPE.ps1

--- WINDOWS/POLICYWIPE.ps1
+++ WINDOWS/POLICYWIPE.ps1
@@ -3,3 +3,4 @@ secedit /configure /cfg %windir%\inf\defltbase.inf /db defltbase.sdb /verbose
 RD /S /Q "%WinDir%\System32\GroupPolicy"
 RD /S /Q "%WinDir%\System32\GroupPolicyUsers"
 gpupdate.exe /force
+sfc.exe /scannow
