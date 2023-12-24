ac7fa316a10da8bbfc2dce691a5cfaf600d8d906
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2023-12-24 13:51:10 -0500

    Update autoUser.ps1

--- WINDOWS/autoUser.ps1
+++ WINDOWS/autoUser.ps1
@@ -2,40 +2,46 @@
 $usersFilePath = "users.txt"
 
 # Read the content of the file
-$usersFileContent = Get-Content -Path $usersFilePath
+$usersFileContent = Get-Content -Path $usersFilePath -ErrorAction SilentlyContinue
+
+# Check if content was read successfully
+if ($null -eq $usersFileContent) {
+    Write-Error "The file at $usersFilePath could not be found or read."
+    return
+}
 
 # Separate the content into administrators and users
 $adminsStart = $usersFileContent.IndexOf("Authorized Administrators") + 1
 $usersStart = $usersFileContent.IndexOf("Authorized Users") + 1
 
-$authorizedAdmins = $usersFileContent[$adminsStart..($usersStart-3)].Trim() | Where-Object { $_ -ne "" }
-$authorizedUsers = $usersFileContent[$usersStart..($usersFileContent.Count-1)].Trim() | Where-Object { $_ -ne "" }
+$authorizedAdmins = $usersFileContent[$adminsStart..($usersStart-3)].Trim() | Where-Object { $_ }
+$authorizedUsers = $usersFileContent[$usersStart..($usersFileContent.Count-1)].Trim() | Where-Object { $_ }
 
 # Get the list of current user accounts on the system, excluding default accounts
 $currentUserAccounts = Get-LocalUser | Where-Object { $_.Name -notmatch "^(Administrator|DefaultAccount|Guest|WDAGUtilityAccount)$" } | Select-Object -ExpandProperty Name
 
 # Add missing authorized users
-$missingUsers = $authorizedUsers | Where-Object { $_ -and $currentUserAccounts -notcontains $_ }
+$missingUsers = $authorizedUsers | Where-Object { $currentUserAccounts -notcontains $_ }
 foreach ($user in $missingUsers) {
-    # Add the user here with appropriate cmdlets, e.g., New-LocalUser
+    New-LocalUser -Name $user -NoPassword -AccountNeverExpires -UserMayNotChangePassword -PasswordNeverExpires
 }
 
 # Remove unauthorized users
 $unauthorizedUsers = $currentUserAccounts | Where-Object { $authorizedUsers -notcontains $_ -and $authorizedAdmins -notcontains $_ }
 foreach ($user in $unauthorizedUsers) {
-    # Remove the user here with appropriate cmdlets, e.g., Remove-LocalUser
+    Remove-LocalUser -Name $user
 }
 
 # Ensure only authorized admins are in the Administrators group
 $currentAdmins = Get-LocalGroupMember -Group "Administrators" | Select-Object -ExpandProperty Name
 $unauthorizedAdmins = $currentAdmins | Where-Object { $authorizedAdmins -notcontains $_ -and $_ -notmatch "^(Administrator|DefaultAccount|Guest|WDAGUtilityAccount)$" }
 foreach ($admin in $unauthorizedAdmins) {
-    # Remove the user from the Administrators group here, e.g., Remove-LocalGroupMember
+    Remove-LocalGroupMember -Group "Administrators" -Member $admin
 }
 
 $missingAdmins = $authorizedAdmins | Where-Object { $currentAdmins -notcontains $_ }
 foreach ($admin in $missingAdmins) {
-    # Add the user to the Administrators group here, e.g., Add-LocalGroupMember
+    Add-LocalGroupMember -Group "Administrators" -Member $admin
 }
 
 # Output completion message
