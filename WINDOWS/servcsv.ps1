61c24a8e95e4347c8b97136a631bb26e2b3c7483
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2023-12-24 01:28:06 -0500

    Update servcsv.ps1

--- WINDOWS/servcsv.ps1
+++ WINDOWS/servcsv.ps1
@@ -1,28 +1,23 @@
 # Path to the CSV file
-$csvPath = "path\to\Black Vipers Windows 10 Service Configurations - Black Viper BlackViper.Com.csv"
-
-# Prompt for the configuration to apply
-$configurations = @("DEFAULT Windows 10 Home", "DEFAULT Windows 10 Pro", "Safe for DESKTOP", "Safe for LAPTOP or TABLET", "Tweaked for DESKTOP")
-$selectedConfig = $host.ui.PromptForChoice("Select Configuration", "Choose the configuration to apply:", $configurations, 0)
+$csvPath = "services.csv"
 
 # Read the CSV file
-$serviceSettings = Import-Csv -Path $csvPath
+$serviceSettings = Import-Csv -Path $csvPath -Header "ServiceName", "StartupType"
 
 # Iterate through each row and set the service configuration
 foreach ($service in $serviceSettings) {
-    $serviceName = $service.'Service Name (Registry)'
-    $startupType = $service[$configurations[$selectedConfig]]
-
-    # Skip if the service is not available or if the startup type is not a standard type
-    if ($startupType -eq "Not Available" -or $startupType -eq "Not Installed" -or $startupType -like "* *") {
-        continue
-    }
+    $serviceName = $service.ServiceName
+    $startupType = $service.StartupType
 
     # Check if the service exists
     $serviceExists = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
     if ($serviceExists) {
-        # Set the startup type of the service
-        Set-Service -Name $serviceName -StartupType $startupType -ErrorAction SilentlyContinue
+        try {
+            # Attempt to set the startup type of the service
+            Set-Service -Name $serviceName -StartupType $startupType -ErrorAction Stop
+        } catch {
+            Write-Warning "Could not set startup type for service $($serviceName)."
+        }
     } else {
         Write-Warning "Service $($serviceName) not found."
     }
