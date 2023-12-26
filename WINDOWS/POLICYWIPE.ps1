530e71dc203b5ddd9eee3f9e19f2eb72b54ddd71
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2023-12-26 18:53:58 -0500

    Update POLICYWIPE.ps1

--- WINDOWS/POLICYWIPE.ps1
+++ WINDOWS/POLICYWIPE.ps1
@@ -1,6 +1,23 @@
-cd C:\Windows\System32
-secedit /configure /cfg %windir%\inf\defltbase.inf /db defltbase.sdb /verbose
-RD /S /Q "%WinDir%\System32\GroupPolicy"
-RD /S /Q "%WinDir%\System32\GroupPolicyUsers"
-gpupdate.exe /force
-sfc.exe /scannow
+# PowerShell Script to Reset Local Group Policy to Default
+
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "Please run this script as an Administrator!"
+    break
+}
+
+
+Stop-Service -Name "gpsvc" -Force
+
+$policyPaths = @("$env:windir\System32\GroupPolicy", "$env:windir\System32\GroupPolicyUsers")
+foreach ($path in $policyPaths) {
+    if (Test-Path $path) {
+        Remove-Item -Path $path -Recurse -Force
+    }
+}
+
+gpupdate /force
+
+Start-Service -Name "gpsvc"
+
+Write-Host "Group Policy has been reset to default." -ForegroundColor Green
