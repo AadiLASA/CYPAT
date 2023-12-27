d377475db5fa4b02a5cf7c15e524b3cc0d981cf7
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2023-12-26 21:58:20 -0800

    changes to todo and main script

--- WINDOWS/altoidkiller.ps1
+++ WINDOWS/altoidkiller.ps1
@@ -5,26 +5,26 @@ Start-Sleep -seconds 5
 
 #Call Scripts
 #User Auditing
-& 'autoUser.ps1'
+& './autoUser.ps1'
 #Logging
-& 'logger.ps1'
+& './logger.ps1'
 #File Finder
-& 'susFinder.ps1'
+& './susFinder.ps1'
 #Application Auditing
-& 'appmgmr.ps1'
+& './appmgmr.ps1'
 #Delete Policies
-& 'POLICYWIPE.ps1'
+& './POLICYWIPE.ps1'
 #Download Hardening Kitty
-& 'hardeningkitty.ps1'
+& './hardeningkitty.ps1'
 #Run Faraday Script
-& 'scriptAutoRun.ps1'
+& './scriptAutoRun.ps1'
 #Execute LGPO
 ./LGPO.exe /g '../{AC9CB38C-EE4E-46BB-93EC-C655C5CF3138}'
 #Chrome Appsec
-& 'chromesecurity.ps1'
+& './chromesecurity.ps1'
 #Service Config
-& 'services.ps1'
-& 'servcsv.ps1'
+& './services.ps1'
+& './servcsv.ps1'
 
 Write-Host "SCRIPTS COMPLETED GG's. RESTART TO APPLY ALL CHANGES"
 Start-Sleep -seconds 5
