aca70a7c6fc842e8976323a23942dab99bd28cd6
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2023-12-29 18:49:08 -0500

    Update POLICYWIPE.ps1

--- WINDOWS/POLICYWIPE.ps1
+++ WINDOWS/POLICYWIPE.ps1
@@ -22,26 +22,4 @@ foreach ($path in $registryPolPaths) {
 gpupdate /force
 
 # Output completion message
-Write-Host "Group Policy has been reset to default. A system restart might be required." -ForegroundColor Green
-
-
-        auditpol /set /category:"Account Logon" /success:enable 
-        auditpol /set /category:"Account Logon" /failure:enable
-        auditpol /set /category:"Account Management" /success:enable
-        auditpol /set /category:"Account Management" /failure:enable
-        auditpol /set /category:"DS Access" /success:enable
-        auditpol /set /category:"DS Access" /failure:enable
-        auditpol /set /category:"Logon/Logoff" /success:enable
-        auditpol /set /category:"Logon/Logoff" /failure:enable
-        auditpol /set /category:"Object Access" /success:enable
-        auditpol /set /category:"Object Access" /failure:enable
-        auditpol /set /category:"Policy Change" /success:enable
-        auditpol /set /category:"Policy Change" /failure:enable
-        auditpol /set /category:"Privilege Use" /success:enable
-        auditpol /set /category:"Privilege Use" /failure:enable
-        auditpol /set /category:"Detailed Tracking" /success:enable
-        auditpol /set /category:"Detailed Tracking" /failure:enable
-        auditpol /set /category:"System" /success:enable 
-        auditpol /set /category:"System" /failure:enable
-
-Write-Host "Additional Audit Policies Applied B)" -ForegroundColor Green
+Write-Host "Group Policy has been reset to default. A system restart might be required." -ForegroundColor Gree
