207a0133faef26c657ec048016dfcc9fdf48acc4
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2024-01-01 11:29:17 -0800

    remove susfinder

--- WINDOWS/scriptAutoRun.ps1
+++ WINDOWS/scriptAutoRun.ps1
@@ -189,32 +189,7 @@ function servicesFunc(){
     }
 }
 
-function susFinder(){try{
-    $path2=Split-Path -parent $MyInvocation.MyCommand.Definition
-	$path=Get-Content $path2\path.txt
 
-	Write-host "Searching for unauthorized files..."
-	$extensions =@("aac","ac3","avi","aiff","bat","bmp","exe","flac","gif","jpeg","jpg","mov","m3u","m4p",
-	"mp2","mp3","mp4","mpeg4","midi","msi","ogg","png","txt","sh","wav","wma","vqf")
-	$tools =@("Cain","nmap","keylogger","Armitage","Wireshark","Metasploit","netcat")
-	Write-host "Checking $extensions"
-	foreach($ext in $extensions){
-		Write-host "Checking for .$ext files"
-		if(Test-path "$path\checkFilesOutput\$ext.txt"){Clear-content "$path\checkFilesOutput\$ext.txt"}
-		C:\Windows\System32\cmd.exe /C dir C:\*.$ext /s /b | Out-File "$path\checkFilesOutput\$ext.txt"
-	}
-	Write-host "Finished searching by extension"
-	Write-host "Checking for $tools"
-	foreach($tool in $tools){
-		Write-host "Checking for $tool"
-		if(Test-path $path\checkFilesOutput\$tool.txt){Clear-content "$path\checkFilesOutput\$tool.txt"}
-		C:\Windows\System32\cmd.exe /C dir C:\*$tool* /s /b | Out-File "$path\checkFilesOutput\$tool.txt"
-}
-}
-catch{
-    Write-Host "SUSFINDER FAIL"
-}
-}
 
 function hostFirewall() {
     Write-Host "Configuring firewall rules..." -ForegroundColor Gray
