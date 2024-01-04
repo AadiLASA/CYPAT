644bfd1839d62016caac0e4ae73a1764d844d5dc
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2024-01-03 21:56:39 -0800

    Update autoSTIG.ps1

--- WINDOWS/autoSTIG.ps1
+++ WINDOWS/autoSTIG.ps1
@@ -1,8 +1,8 @@
 #install git
 choco install git
 #create directory
-mkdir "Standalone-Windows-STIG-Script"
-cd "Standalone-Windows-STIG-Script"
+mkdir "C:\Cypat-main\Windows\Standalone-Windows-STIG-Script"
+cd "C:\Cypat-main\Windows\Standalone-Windows-STIG-Script"
 #clone repo in current directory
 gh repo clone simeononsecurity/Standalone-Windows-STIG-Script
 #run command
