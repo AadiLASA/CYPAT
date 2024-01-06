8f881a32e5f89f7fce80b8c3c7d92898ba743260
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2024-01-07 02:43:26 +0400

    Update autoSTIG.ps1

--- WINDOWS/autoSTIG.ps1
+++ WINDOWS/autoSTIG.ps1
@@ -1,8 +1,9 @@
 #create directory
 mkdir "C:\Cypat-main\Windows\Standalone-Windows-STIG-Script"
-cd "C:\Cypat-main\Windows\Standalone-Windows-STIG-Script"
 #clone repo in current directory
 gh repo clone simeononsecurity/Standalone-Windows-STIG-Script
+#navigate to directory
+cd "C:\Cypat-main\Windows\Standalone-Windows-STIG-Script"
 #run command
 .\secure-standalone.ps1 -firefox $false
 cd "C:\Cypat-main\Windows"
