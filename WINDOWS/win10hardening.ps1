18abc5eef6f0e921838e61a5b2de2215df82b37c
Aaditya Bhat <115965167+AadiLASA@users.noreply.github.com>
2024-01-07 08:43:31 +0400

    Update win10hardening.ps1

--- WINDOWS/win10hardening.ps1
+++ WINDOWS/win10hardening.ps1
@@ -1,3 +1,4 @@
 Install-Module -Name 'Harden-Windows-Security-Module' -Force
 Write-Host "Utilizing Additional Hardening Script" 
 Protect-WindowsSecurity
+#Additional Information : https://hotcakex.github.io/#apply-the-latest-hardening-measures-directly-from-this-github-repository
